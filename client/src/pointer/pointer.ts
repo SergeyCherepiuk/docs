decf99049ce9c27e9e7d60cf9a1cbc83ded4bb66
Sergey Cherepiuk <s.cherepiuk@gmail.com>
2023-10-08 08:25:34 +0200

    DOCS-2: Better pointer icon

--- client/src/pointer/pointer.ts
+++ client/src/pointer/pointer.ts
@@ -1,21 +1,5 @@
-// TODO: [0, 0] should be center of a screen
-type Position = {
-    x: number,
-    y: number,
-}
-
-function randomColor(): string {
-    return "#" + Math.floor(Math.random()*16777215).toString(16);
-}
+export type Position = { x: number, y: number }
 
 export class Pointer {
-    color: string
-
-    constructor (
-        public id: string,
-        color: string | undefined,
-        public position: Position,
-    ) {
-        this.color = color ? color : randomColor()
-    }    
+    constructor (public id: string, public position: Position) {}
 }
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { onMount } from "svelte";
-	import { writable } from "svelte/store";
+	import { get, writable } from "svelte/store";
 	import { Pointer } from "../pointer/pointer";
 	import { v1 as uuid } from "uuid";
 	import type { Message } from "../ws/message";
@@ -17,28 +17,43 @@
             if (message.type == "pointer-movement") {
                 let pointer = message.payload as Pointer
                 pointers.update(map => map.set(pointer.id, pointer))
+
+                let image = document.getElementById(`pointer-${pointer.id}`) as HTMLImageElement
+                if (image) {
+                    image.style.left = pointer.position.x + "px"
+                    image.style.top = pointer.position.y + "px"
+                }
             } else if (message.type == "content-change") {
                 content.update(() => message.payload as string)
             }
         }
 
-        let plain = document.getElementById("plain") as HTMLDivElement
-        plain.onmousemove = e => {
-            let pointer = new Pointer(
-                id, $pointers.get(id)?.color, { x: e.clientX, y: e.clientY },
-            )
-            pointers.update(map => map.set(pointer.id, pointer))
-
-            let message = { type: "pointer-movement", payload: pointer }
-            send(socket, message)
+        let area = document.getElementById("area") as HTMLTextAreaElement
+        area.onmousemove = e => {
+            let pointer = new Pointer(id, {
+                x: e.clientX + window.scrollX,
+                y: e.clientY + window.scrollY
+            })
+            sendPointer(socket, pointer)
+        }
+        window.onscroll = () => {
+            let pointer = get(pointers).get(id)
+            if (pointer) {
+                sendPointer(socket, pointer)
+            }
         }
 
         content.subscribe(c => {
-            console.log(c)
             let message = { type: "content-change", payload: c}
             send(socket, message)
         })
-    })    
+    })
+
+    function sendPointer(socket: WebSocket, pointer: Pointer) {
+        pointers.update(map => map.set(id, pointer))
+        let message = { type: "pointer-movement", payload: pointer }
+        send(socket, message)
+    }
 
     function send(socket: WebSocket, value: any) {
         if (socket.readyState == socket.OPEN) {
@@ -47,13 +62,9 @@
     }
 </script>
 
-<div id="plain" class="w-full h-full bg-gray-300 text-center">
-    <svg class="absolute w-full h-full pointer-events-none">
-        {#each $pointers.values() as pointer}
-            {#if pointer.id != id}
-                <circle cx={pointer.position.x} cy={pointer.position.y} r=10 fill={pointer.color} />
-            {/if}
-        {/each}
-    </svg>
-    <textarea id="area" class="w-[768px] h-full resize-none p-8" bind:value={$content} />
-</div>
+{#each $pointers.values() as pointer}
+    {#if pointer.id != id}
+        <img id={`pointer-${pointer.id}`} src="/pointer.svg" alt="Pointer" class="absolute pointer-events-none" />
+    {/if}
+{/each}
+<textarea id="area" class="w-[768px] h-[2000px] resize-none p-8" bind:value={$content} />
